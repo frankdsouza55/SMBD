34fa908ab6fab17d35e1a721a5e16ddb559ad2e5
Frank <frankdsouza55@gmail.com>
2017-10-19 19:37:40 +0530

    Add files via upload
    This is the main login page which links to all the remaining forms.

---
+++ Login/Chef/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chef
+{
+    public partial class Chef : Form
+    {
+        public Chef()
+        {
+            InitializeComponent();
+        }
+
+        private void Chef_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Login/Login/LogIn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace Login
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            int x = Int32.Parse(textBox1.Text);
+            if (x>=100 && x <200 && textBox2.Text =="chef")
+            {
+                Chef obj1 = new Chef();
+                obj1.Show();
+            }
+            else if (x>=200 && x < 300 && textBox2.Text == "waiter")
+            {
+                Waiter obj2 = new Waiter();
+                obj2.Show();
+            }
+            else if (x >= 300 && x < 400 && textBox2.Text == "cashier")
+            {
+                Cashier obj3 = new Cashier();
+                obj3.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username  or password");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Customer obj4 = new Customer();
+            obj4.Show();
+        }
+    }
+}
